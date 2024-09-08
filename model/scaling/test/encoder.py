feat(model/scaling): 修复local attention的bug，添加了long attention支持

model/scaling/test/encoder.py
@@ -6,29 +6,29 @@ from model.scaling.transformer_layer import TransformerLayer
 
 
 class TestEncoder(unittest.TestCase):
-    # def test_encode(self):
-    #     # TODO: Implement test for encode method
-    #     encoder = TransformerLayer(1, 3)
-    #     input = torch.arange(0, 36, dtype=torch.float32).reshape(1, 1, 6, 6)
-    #     output = encoder(input)
-    #     print(output.shape)
+    def test_encode(self):
+        # TODO: Implement test for encode method
+        encoder = TransformerLayer(1, 3)
+        input = torch.arange(0, 36, dtype=torch.float32).reshape(1, 1, 6, 6)
+        output = encoder(input)
+        print(output.shape)
 
-    # def test_padding(self):
-    #     encoder = TransformerLayer(1, 3)
-    #     input = torch.arange(0, 25, dtype=torch.float32).reshape(1, 1, 5, 5)
-    #     output = encoder(input)
-    #     print(output.shape)
-    #     input = torch.arange(0, 20, dtype=torch.float32).reshape(1, 1, 4, 5)
-    #     output = encoder(input)
-    #     print(output.shape)
-    #     input = torch.arange(0, 20, dtype=torch.float32).reshape(1, 1, 5, 4)
-    #     output = encoder(input)
-    #     print(output.shape)
-    #     encoder = TransformerLayer(1, 7)
-    #     input = torch.arange(
-    #         0, 27 * 29, dtype=torch.float32).reshape(1, 1, 27, 29)
-    #     output = encoder(input)
-    #     print(output.shape)
+    def test_padding(self):
+        encoder = TransformerLayer(1, 3)
+        input = torch.arange(0, 25, dtype=torch.float32).reshape(1, 1, 5, 5)
+        output = encoder(input)
+        print(output.shape)
+        input = torch.arange(0, 20, dtype=torch.float32).reshape(1, 1, 4, 5)
+        output = encoder(input)
+        print(output.shape)
+        input = torch.arange(0, 20, dtype=torch.float32).reshape(1, 1, 5, 4)
+        output = encoder(input)
+        print(output.shape)
+        encoder = TransformerLayer(1, 7)
+        input = torch.arange(
+            0, 27 * 29, dtype=torch.float32).reshape(1, 1, 27, 29)
+        output = encoder(input)
+        print(output.shape)
 
     def test_correct_reshape(self):
         class ByPass(nn.Module):
@@ -47,18 +47,16 @@ class TestEncoder(unittest.TestCase):
                 )
 
             def forward(self, x):
-                print(self.groups)
                 return super(MockedTransformer, self).forward(x)
 
-        encoder = MockedTransformer(1, 3)
-        input = torch.arange(0, 72, dtype=torch.float32).reshape(1, 2, 6, 6)
-        # output = encoder(input)
-        # assert input.equal(output)
-        # print(input, "and ", encoder.groups)
+        input = torch.arange(
+            0, 2 * 2 * 6 * 6, dtype=torch.float32).reshape(2, 2, 6, 6)
+        encoder = MockedTransformer(1, 3, 'local')
+        output = encoder(input)
+        assert input.equal(output)
         encoder = MockedTransformer(2, 3, 'long')
         output = encoder(input)
-        # assert input.equal(output)
-        # print(input, "and", encoder.groups)
+        assert input.equal(output)
 
 
 if __name__ == '__main__':

model/scaling/transformer_layer.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class TransformerLayer(nn.Module):
-    def __init__(self, input_dim: int, group=3, attention_type="local"):
+    def __init__(self, input_dim: int, group=3, attention_type="long"):
         super(TransformerLayer, self).__init__()
         self.input_dim = input_dim
         self.group = group
@@ -41,53 +41,42 @@ class TransformerLayer(nn.Module):
 
     def forward(self, x):
         x = self.padding(x)
+        bs, c, h, w = x.shape
         if self.attention_type == "local":
-            group_list = torch.split(x, self.group, dim=2)
-            group_list = sum(map(lambda e: torch.split(
-                e, self.group, dim=3), group_list), ())
-            # now the groups' shape is [B * H * W // (G * G), C, G, G]
-            groups = torch.cat(group_list, dim=0)
-            # then we transform the groups to the standard tokens
-            # [B * H * W // (G * G), G * G, C]
-            groups = groups.flatten(2, 3).permute(0, 2, 1)
-
-            self.groups = groups
-            print(groups.shape, self.position_embedding.shape)
-
-            # groups with position embedding
+            groups = F.unfold(x, kernel_size=(self.group, self.group),
+                              stride=(self.group, self.group))
+            groups = groups.reshape(bs, c, self.group * self.group, -1)
+            groups = groups.permute(0, 3, 2, 1)
+            groups = groups.reshape(-1, self.group * self.group, c)
             groups += self.position_embedding
             groups = self.encoder(groups)
-            # then we transform the tokens back to the groups
-
-            # [B, H // G, W // G, G, G, C]
-            groups = groups.reshape(-1,
-                                    x.shape[2] // self.group,
-                                    x.shape[3] // self.group,
-                                    self.group, self.group, self.input_dim)
-            # [B, H, W, C]
-            groups = groups.permute(
-                0, 1, 3, 2, 4, 5).reshape(-1, x.shape[2], x.shape[3], self.input_dim)
-            groups = groups.permute(0, 3, 1, 2)
-            assert groups.shape == x.shape
-            return groups
+            groups = groups.reshape(bs, -1, self.group * self.group, c)
+            groups = groups.permute(0, 3, 2, 1)
+            groups = groups.reshape(bs, c * self.group * self.group, -1)
+            y = F.fold(groups, output_size=(h, w),
+                       kernel_size=(self.group, self.group),
+                       stride=(self.group, self.group))
+            assert y.shape == x.shape
+            return y
         elif self.attention_type == "long":
-            stride = (x.shape[2] // self.group, x.shape[3] // self.group)
-
+            stride = (h // self.group, w // self.group)
             # project the groups
             groups = F.unfold(x, kernel_size=stride,
-                              stride=(stride))
-            groups = groups.reshape(-1, self.input_dim,
-                                    groups.shape[1] // self.input_dim, groups.shape[2])
-            # groups = groups.permute(0, 1, 3, 2)
-            groups = groups.reshape(-1, self.input_dim,
-                                    self.group * self.group)
-            groups = groups.permute(0, 2, 1)
+                              stride=stride)
+            groups = groups.reshape(
+                bs, c, stride[0] * stride[1], self.group * self.group)
+            groups = groups.permute(0, 2, 3, 1)
+            groups = groups.reshape(-1, self.group * self.group, c)
             groups += self.position_embedding
             groups = self.encoder(groups)
-            groups = groups.permute(0, 2, 1)
-            # groups = groups.reshape(-1, self.input_dim,
-            #                         self.group, self.group)
-            print(x, groups)
-            return groups
+            groups = groups.reshape(bs, stride[0] * stride[1],
+                                    self.group * self.group, c)
+            groups = groups.permute(0, 3, 1, 2)
+            groups = groups.reshape(bs, c * stride[0] * stride[1],
+                                    self.group * self.group)
+            y = F.fold(groups, output_size=(h, w), kernel_size=stride,
+                       stride=stride)
+            assert y.shape == x.shape
+            return y
         else:
             raise NotImplementedError("please check the attention type")