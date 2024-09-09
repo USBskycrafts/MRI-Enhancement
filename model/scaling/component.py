6fded6d2fed69bf7425e908dded5849eb9def88e
junran.young <usbskycrafts@outlook.com>
2024-09-09 12:12:37 +0800

    feat(model/scaling): 添加了CrossFormer整体网络的测试，修复了TransformerUnit里的bug

--- model/scaling/component.py
+++ model/scaling/component.py
@@ -5,21 +5,55 @@ from .cross_embedding import CrossScaleEmbedding
 from typing import Dict, Any, List
 
 
+class CrossScaleParams:
+    def __init__(self, input_dim: int,
+                 output_dim: int,
+                 kernel_size: List[int],
+                 stride: int):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+    def keys(self):
+        return ['input_dim', 'output_dim', 'kernel_size', 'stride']
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+
+class TransformerParams:
+    def __init__(self, input_dim: int,
+                 group: int,
+                 n_layer: int):
+        self.input_dim = input_dim
+        self.group = group
+        self.n_layer = n_layer
+
+    def keys(self):
+        return ['input_dim', 'group', 'n_layer']
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+
 class TransformerUnit(nn.Module):
-    def __init__(self, cross_scale_params: Dict[str, Any], transformer_params: Dict[str, Any]):
+    def __init__(self, cross_scale_params: CrossScaleParams, transformer_params: TransformerParams):
         super(TransformerUnit, self).__init__()
-        self.encoder_embedding = CrossScaleEmbedding(**cross_scale_params)
-        self.encoder_layers = TransformerPack(**transformer_params)
+        self.encoder_embedding = CrossScaleEmbedding(
+            **dict(cross_scale_params))
+        self.encoder_layers = TransformerPack(**dict(transformer_params))
         self.decoder_embedding = CrossScaleEmbedding(
             **cross_scale_params, reversed=True)
-        self.decoder_layers = TransformerPack(**transformer_params)
+        transformer_params.input_dim = cross_scale_params.input_dim
+        self.decoder_layers = TransformerPack(**dict(transformer_params))
 
     def forward(self, x, next: List[nn.Module]):
         h = self.encoder_embedding(x)
         h = self.encoder_layers(h)
 
-        first, rest = next[0], next[1:]
-        if first is not None:
+        if len(next) > 0:
+            first, rest = next[0], next[1:]
             h_next = first(h, rest)
         else:
             h_next = h
---
+++ model/scaling/test/component.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import unittest
+from model.scaling.component import TransformerUnit, CrossScaleParams, TransformerParams
+from .utils import splitter, timer
+
+
+class Crossformer(nn.Module):
+    def __init__(self):
+        super(Crossformer, self).__init__()
+
+        self.cross_params = [
+            CrossScaleParams(
+                input_dim=3,
+                output_dim=64,
+                kernel_size=[4, 8, 16, 32],
+                stride=4),
+            CrossScaleParams(
+                input_dim=64,
+                output_dim=128,
+                kernel_size=[2, 4],
+                stride=2),
+        ]
+        self.transformer_params = [
+            TransformerParams(
+                input_dim=64,
+                group=7,
+                n_layer=1
+            ),
+            TransformerParams(
+                input_dim=128,
+                group=7,
+                n_layer=1
+            ),
+        ]
+
+        self.transformer_units = nn.ModuleList([])
+        for cross_param, transformer_param in zip(self.cross_params, self.transformer_params):
+            self.transformer_units.append(
+                TransformerUnit(cross_param, transformer_param))
+
+    def forward(self, x):
+        first, rest = self.transformer_units[0], list(
+            self.transformer_units)[1:]
+        return first(x, rest)
+
+
+class TestCrossformer(unittest.TestCase):
+    @splitter
+    @timer
+    def test_regular_shape(self):
+        self.crossformer = Crossformer()
+        x = torch.randn(1, 3, 224, 224)
+        y = self.crossformer(x)
+        print(x.shape, y.shape)
+
+    @splitter
+    @timer
+    def test_irregular_shape(self):
+        self.crossformer = Crossformer()
+        x = torch.randn(1, 3, 240, 240)
+        y = self.crossformer(x)
+        print(x.shape, y.shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
